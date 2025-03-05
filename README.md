82e0327ee9e917e1be3c59472151f7d17dcedcda
zjx <772109739@qq.com>
2025-03-05 13:03:09 +0800

    更新

---
+++ README.md
@@ -0,0 +1,24 @@
+# IPTV
+此项目通过其它直播源抓取集成一个总直播源，直播内容通过demo自定义进行运行爬取。每天自动更新直播，提供在线地址，可以直接使用各大播放平台。M3U配置peg和台标，支持IPV6/4双栈访问!
+> 声明: 所有播放源均收集于互联网，仅供测试研究学习，`不得商用!`
+## TVBox
+-
+## 直播源调用地址
+- 国内M3U [每三日更新](http://175.178.251.183:6689/live.m3u)
+- 国内TXT [每三日更新](http://175.178.251.183:6689/live.txt)
+- 仓库文件加速 [线路1](https://gh.con.sh/https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.m3u)
+- 仓库文件加速 [线路2](https://cdn.jsdelivr.net/gh/yuanzl77/IPTV@latest/live.m3u)
+
+#
+![image](/image/Screenshot_2024-07-24-21-20-39-458_com.github.tvbox.osc.tk.jpg)
+[video](https://youtu.be/HMjiSJHXD8Y?si=yb8FuoG9mR2aLoQW)
+
+## IPV6优势
+
+
+# 感谢您的支持与打赏
+![打赏图](https://raw.githubusercontent.com/yuanzl77/zafu/main/打赏码.png)
+**您的打赏不仅是对我的肯定，更是我前进的动力，感谢您的支持！**
+
+## Star History
+[![Stargazers over time](https://starchart.cc/yuanzl77/IPTV.svg?variant=adaptive)](https://starchart.cc/yuanzl77/IPTV)
---
+++ config.py
@@ -0,0 +1,72 @@
+ip_version_priority = "iptv"
+
+source_urls = [
+    "http://jianhan.vip/yzt91220/幻影直播.txt",
+    "https://live.iptv365.org/live.txt",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+]
+
+url_blacklist = [
+    "epg.pw/stream/",
+    "103.40.13.71:12390",
+    "[2409:8087:1a01:df::4077]/PLTV/",
+    "8.210.140.75:68",
+    "154.12.50.54",
+    "yinhe.live_hls.zte.com",
+    "8.137.59.151",
+    "[2409:8087:7000:20:1000::22]:6060",
+    "histar.zapi.us.kg",
+    "www.tfiplaytv.vip",
+    "dp.sxtv.top",
+    "111.230.30.193",
+    "148.135.93.213:81",
+    "live.goodiptv.club",
+    "iptv.luas.edu.cn",
+    "[2409:8087:2001:20:2800:0:df6e:eb22]:80",
+    "[2409:8087:2001:20:2800:0:df6e:eb23]:80",
+    "[2409:8087:2001:20:2800:0:df6e:eb1d]/ott.mobaibox.com/",
+    "[2409:8087:2001:20:2800:0:df6e:eb1d]:80",
+    "[2409:8087:2001:20:2800:0:df6e:eb24]",
+    "2409:8087:2001:20:2800:0:df6e:eb25]:80",
+    "[2409:8087:2001:20:2800:0:df6e:eb27]"
+]
+
+announcements = [
+    {
+        "channel": "公告",
+        "entries": [
+            {"name": "请阅读", "url": "https://apd-vlive.apdcdn.tc.qq.com/vhot2.qqvideo.tc.qq.com/AushHwnetjq70sqyalY5XtqgyoE27STTmHONxHfHnB6k/B_JxNyiJmktHRgresXhfyMepo6vx8WVcdyfuHuv6uJKag_K2HP_LebFs1QZh2CGVlb/svp_50200/gzc_1000195_0bc3ymax4aabw4aptgmiozrrtq6ep3bqc7sa.f2.mp4?sdtfrom=v1010&guid=dcf3b67caa37bbef&vkey=C717BB183A01B676160FCAFA81E053F20961D34DE903F4C49A9E21567E278BE1F87362C00CEF656658DDBE80DF0C1FAE44C1805289B7D276A46E131E59CB5E7E3C4CA0095778082F17A13E8410D0C0AA9A257D6EB35DCCCA41F35BFEE9C4C3A62D48081A6C4FD16E3315F8F0132BEA7AF9C458040239A17074E9869FFAB9010AE12A1D4616666D61EB4C0A7FA299C0F5172780A07A3DC22622978B7D1E27AE6C49FAF2E1E287B390", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": "我的主页", "url": "https://canghaixiao369.github.io/canghaixiao.github.io/", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": "更新日期", "url": "https://apd-ugcvlive.apdcdn.tc.qq.com/om.tc.qq.com/AfRQEqU8M4RuSFlAUY0J-trbzFrodh6a9DsHQeZp-UR4/B_JxNyiJmktHRgresXhfyMerY_o4PBzsUHlVBFCs5Pi4vSYX7gzUB7kvrtMNFHHxng/svp_50069/gzc_1000035_0bc3cuaa2aaafuabwic5ofqzkfodbukqadka.f204110.mp4?sdtfrom=v1010&guid=dcf3b67caa37bbef&vkey=5738C0C3CAC6914B1B1E37D08371C2C3E0697192ACA840C5AF3C104372C2242A21845A9BF3C973370951822D49CE0392011A7D08AFC55DAD4E4C388CF9EE047CA1133DFFDC7DD132311F9AD5FAA58B866DB1E8628CC68B94149288A9D1E2962F033589DAE8A2CE325C9B720DA5CB31E57122E23F076EBE6BAE82BF167B25007A1C3F8A78A92C6EEBA4ED10B571F87F24C5E47B3A09A2F17AAF6D1D4B148A68F6F9083C715BF91B9D", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": None, "url": "https://apd-ugcvlive.apdcdn.tc.qq.com/om.tc.qq.com/AfRQEqU8M4RuSFlAUY0J-trbzFrodh6a9DsHQeZp-UR4/B_JxNyiJmktHRgresXhfyMerY_o4PBzsUHlVBFCs5Pi4vSYX7gzUB7kvrtMNFHHxng/svp_50069/gzc_1000035_0bc3cuaa2aaafuabwic5ofqzkfodbukqadka.f204110.mp4?sdtfrom=v1010&guid=dcf3b67caa37bbef&vkey=5738C0C3CAC6914B1B1E37D08371C2C3E0697192ACA840C5AF3C104372C2242A21845A9BF3C973370951822D49CE0392011A7D08AFC55DAD4E4C388CF9EE047CA1133DFFDC7DD132311F9AD5FAA58B866DB1E8628CC68B94149288A9D1E2962F033589DAE8A2CE325C9B720DA5CB31E57122E23F076EBE6BAE82BF167B25007A1C3F8A78A92C6EEBA4ED10B571F87F24C5E47B3A09A2F17AAF6D1D4B148A68F6F9083C715BF91B9D", "logo": "http://175.178.251.183:6689/LR.jpg"}
+        ]
+    }
+]
+
+epg_urls = [
+    "https://live.fanmingming.com/e.xml",
+    "http://epg.51zmt.top:8000/e.xml",
+    "http://epg.aptvapp.com/xml",
+    "https://epg.pw/xmltv/epg_CN.xml",
+    "https://epg.pw/xmltv/epg_HK.xml",
+    "https://epg.pw/xmltv/epg_TW.xml"
+]
